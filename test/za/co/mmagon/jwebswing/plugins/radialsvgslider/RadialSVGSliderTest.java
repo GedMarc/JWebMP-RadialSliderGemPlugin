f38205863b0923bf76abfd7e9b9d5f063440e33d
Marc Magon <ged_marc@hotmail.com>
2017-09-28 03:56:00 +0200

    JUnit 5 updates, Pretty Checkboxes implementation and a lot more
    Signed-off-by: Marc Magon <ged_marc@hotmail.com>

--- test/za/co/mmagon/jwebswing/plugins/radialsvgslider/RadialSVGSliderTest.java
+++ test/za/co/mmagon/jwebswing/plugins/radialsvgslider/RadialSVGSliderTest.java
@@ -16,7 +16,7 @@
  */
 package za.co.mmagon.jwebswing.plugins.radialsvgslider;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import za.co.mmagon.BaseTestClass;
 import za.co.mmagon.jwebswing.Page;
 import za.co.mmagon.jwebswing.base.html.Div;
