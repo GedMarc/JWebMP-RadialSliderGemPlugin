0f7e4059a6e6a3a02fe193f93d68da975613def7
GedMarc <ged_marc@hotmail.com>
2019-01-17 22:00:34 +0200

    GPG Update

--- src/main/java/com/jwebmp/plugins/radialsvgslider/RadialSVGSlider.java
+++ src/main/java/com/jwebmp/plugins/radialsvgslider/RadialSVGSlider.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017 Marc Magon
+ * Copyright (C) 2017 GedMarc
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -31,7 +31,7 @@ import javax.validation.constraints.NotNull;
  * Customized to be generic. https://codyhouse.co/gem/radial-svg-slider/
  * <p>
  *
- * @author Marc Magon
+ * @author GedMarc
  * @version 1.0
  * @since 29 Aug 2015
  */
