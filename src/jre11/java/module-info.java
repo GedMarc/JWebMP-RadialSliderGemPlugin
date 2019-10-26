5ea4a1b1ddecbfa0aa7247b581ad73ce4e88bfb4
GedMarc <ged_marc@hotmail.com>
2019-10-26 02:35:26 +0200

    Guiced EE Migration to its own space

--- src/jre11/java/module-info.java
+++ src/jre11/java/module-info.java
@@ -3,16 +3,16 @@ module com.jwebmp.plugins.radialsvgslider {
 	exports com.jwebmp.plugins.radialsvgslider;
 
 	requires com.jwebmp.core;
-	requires com.jwebmp.logmaster;
+	requires com.guicedee.logmaster;
 	requires com.fasterxml.jackson.annotation;
 
 	requires java.validation;
 	requires java.logging;
-	requires com.jwebmp.guicedinjection;
+	requires com.guicedee.guicedinjection;
 
 	provides com.jwebmp.core.services.IPageConfigurator with com.jwebmp.plugins.radialsvgslider.RadialSVGSliderGemPageConfigurator;
-	provides com.jwebmp.guicedinjection.interfaces.IGuiceScanModuleExclusions with com.jwebmp.plugins.radialsvgslider.implementations.RadialSliderExclusionsModule;
-	provides com.jwebmp.guicedinjection.interfaces.IGuiceScanJarExclusions with com.jwebmp.plugins.radialsvgslider.implementations.RadialSliderExclusionsModule;
+	provides com.guicedee.guicedinjection.interfaces.IGuiceScanModuleExclusions with com.jwebmp.plugins.radialsvgslider.implementations.RadialSliderExclusionsModule;
+	provides com.guicedee.guicedinjection.interfaces.IGuiceScanJarExclusions with com.jwebmp.plugins.radialsvgslider.implementations.RadialSliderExclusionsModule;
 
 	opens com.jwebmp.plugins.radialsvgslider to com.fasterxml.jackson.databind, com.jwebmp.core;
 }
